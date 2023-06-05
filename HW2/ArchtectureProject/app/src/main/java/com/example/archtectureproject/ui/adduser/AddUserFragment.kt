8ff97f9fc496362be4430d59f6d834b42c924261
Sahar Blueshtein <blueshtein.sahar@post.runi.ac.il>
2023-06-05 22:05:48 +0300

    added style values and fixed navbar

--- HW2/ArchtectureProject/app/src/main/java/com/example/archtectureproject/ui/adduser/AddUserFragment.kt
+++ HW2/ArchtectureProject/app/src/main/java/com/example/archtectureproject/ui/adduser/AddUserFragment.kt
@@ -14,6 +14,7 @@ import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import com.example.archtectureproject.ui.ChoreViewModel
 import com.example.archtectureproject.R
@@ -80,7 +81,7 @@ class AddUserFragment : Fragment() {
                     else -> false
                 }
             }
-        })
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 
 
